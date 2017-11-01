edc7ca19fac4ff584462d2090ccf1b1f60aa5308
aegroto <dott.micronesia@gmail.com>
2017-11-01 15:24:51 +0100

    pressure button fix

--- liotgui/src/main/java/com/aegroto/liotgui/clickable/GUIPressureButton.java
+++ liotgui/src/main/java/com/aegroto/liotgui/clickable/GUIPressureButton.java
@@ -3,18 +3,16 @@ package com.aegroto.liotgui.clickable;
 import com.aegroto.liotgui.state.GuiAppState;
 import com.jme3.math.Vector2f;
 
-/**
- * Created by lorenzo on 07/09/17.
- */
-
 public abstract class GUIPressureButton extends GUIButton {
     public GUIPressureButton(String buttonText, Vector2f activeArea, GuiAppState guiAppState) {
         super(buttonText, activeArea, guiAppState);
     }
 
-    public void onLeft() { }
+    @Override
+    public void onLeft(Vector2f mousePos) { }
 
-    public void onContinuedClick() {
+    @Override
+    public void onContinuedClick(Vector2f mousePos) {
         execFunction();
     }
 }
